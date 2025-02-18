e3014f4965ed78d9d0a3aab2510b761b03e3e246
juanlopcon07 <juan.21futmas07@gmail.com>
2025-02-17 20:04:09 -0500

    Modificacion de atributos, propiedades, texto y estilos

---
+++ Manipulating-Elements/Atributes&Properties/app.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('input');
+input.value = 'Apellido'
+
+const title = document.querySelector('#app-title');
+title.textContent = 'Nuevo texto'
+title.innerText = 'Este es otro titulo'
+
---
+++ Manipulating-Elements/StylingElements/app.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('h1')
+title.style.color = 'red'
+
+const menu = document.querySelector('menu')
+menu.style.backgroundColor = 'red'
+menu.style.fontSize = '25px'
+menu.className = 'main-menu'
