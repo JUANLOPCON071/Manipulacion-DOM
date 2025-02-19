d603b7a5974736ee76552be02546357e63b6fa95
juanlopcon07 <juan.21futmas07@gmail.com>
2025-02-18 20:47:30 -0500

    Modificar visibilidad de elementos HTML

--- Manipulating-Elements/StylingElements/app.js
+++ Manipulating-Elements/StylingElements/app.js
@@ -5,3 +5,7 @@ const menu = document.querySelector('menu')
 menu.style.backgroundColor = 'red'
 menu.style.fontSize = '25px'
 menu.className = 'main-menu'
+
+button.addEventListener('click', () => { 
+    menu.classList.toggle('invisible');
+});
